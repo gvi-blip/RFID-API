ec2d02be8eee568e9185f99e4982763e2a972f99
jolstorm <gaurav.virmani@sparkeighteen.com>
2022-07-05 16:37:54 +0530

    ft:Add CORS

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const cors = require('cors');
 const globalErrorController = require('./controllers/errorController');
 const apiuserRouter = require('./routes/apiuserRoutes');
 const userRouter = require('./routes/usersRoutes');
@@ -13,9 +14,11 @@ const AppError = require('./utilities/appError');
 dotenv.config({ path: path.resolve('config.env') });
 
 const app = express();
+
 app.use(express.json());
 app.use(express.Router());
 app.use(morgan('dev'));
+app.use(cors({ origin: '*' }));
 
 app.use('/api/v1/user', authController.protect, userRouter);
 app.use('/api/v1/apiuser', apiuserRouter);
